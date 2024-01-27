a9c2db624efc45903d1104ae59e2ccfc6a167d45
duncan-mcclellan <82678263+duncan-mcclellan@users.noreply.github.com>
2024-01-26 21:39:34 -0600

    Project creation

---
+++ Source/MoonRabbit/MoonRabbit.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MoonRabbit.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MoonRabbit, "MoonRabbit" );
